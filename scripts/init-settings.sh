14b01547848972ce30b2ef49fd925315bd005df5
cpuer <lastczj@gmail.com>
2023-05-14 00:57:32 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -10,19 +10,11 @@
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
-# Disable opkg signature check
-# sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
-# 禁用ipv6前缀
+# Disable IPV6 ula prefix
 sed -i 's/^[^#].*option ula/#&/' /etc/config/network
-# Disable autostart by default for some packages
-cd /etc/rc.d
-rm -f S98udptools || true
-rm -f S99nft-qos || true
 
-# Try to execute init.sh (if exists)
-
-if [ ! -f "/boot/init.sh" ]; then
-bash /boot/init.sh
-fi
+# Check file system during boot
+uci set fstab.@global[0].check_fs=1
+uci commit
 
 exit 0
