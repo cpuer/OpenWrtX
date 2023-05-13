8d0406e4a5e1134db9f5be45ecea595835d05ee0
cpuer <lastczj@gmail.com>
2023-05-14 00:51:40 +0800

    Update x86_64.sh

--- scripts/x86_64.sh
+++ scripts/x86_64.sh
@@ -41,22 +41,22 @@ git clone https://github.com/xiaorouji/openwrt-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 
 # Add luci-app-unblockneteasemusic
-rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
-git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+# rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
+# git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 
 # Add luci-proto-minieap
-git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
+# git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 
 # Add luci-app-onliner (need luci-app-nlbwmon)
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+# git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add ddnsto & linkease
-svn export https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
-svn export https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
+# svn export https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
+# svn export https://github.com/linkease/nas-packages/trunk/network/services/ddnsto
 
 # Add OpenClash
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
@@ -69,7 +69,7 @@ svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 # git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 # Add luci-app-poweroff
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
+# git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
 
 # Add luci-theme
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
@@ -81,7 +81,7 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 # git clone https://github.com/DHDAXCW/theme
 
 # Add subconverter
-git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
+# git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # alist
 # git clone https://github.com/sbwml/openwrt-alist --depth=1
@@ -93,7 +93,7 @@ svn export https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
 svn export https://github.com/openwrt/packages/trunk/utils/apk
 
 # Add OpenAppFilter
-git clone --depth=1 https://github.com/destan19/OpenAppFilter
+# git clone --depth=1 https://github.com/destan19/OpenAppFilter
 
 # Add luci-aliyundrive-webdav
 # rm -rf ../../customfeeds/luci/applications/luci-app-aliyundrive-webdav 
@@ -117,10 +117,10 @@ sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-setti
 popd
 
 # Fix libssh
-pushd feeds/packages/libs
-rm -rf libssh
-svn export https://github.com/openwrt/packages/trunk/libs/libssh
-popd
+# pushd feeds/packages/libs
+# rm -rf libssh
+# svn export https://github.com/openwrt/packages/trunk/libs/libssh
+# popd
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
@@ -129,9 +129,9 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Test kernel 6.1
-rm -rf target/linux/x86/base-files/etc/board.d/02_network
-cp -f $GITHUB_WORKSPACE/02_network target/linux/x86/base-files/etc/board.d/02_network
+# rm -rf target/linux/x86/base-files/etc/board.d/02_network
+# cp -f $GITHUB_WORKSPACE/02_network target/linux/x86/base-files/etc/board.d/02_network
 sed -i 's/5.15/6.1/g' target/linux/x86/Makefile
 # rm -rf package/base-files/files/etc/banner
 # wget -P package/base-files/files/etc https://raw.githubusercontent.com/DHDAXCW/lede-rockchip/stable/package/base-files/files/etc/banner
-cp -r ../target/linux/generic/pending-6.1/ ./target/linux/generic/
+# cp -r ../target/linux/generic/pending-6.1/ ./target/linux/generic/
