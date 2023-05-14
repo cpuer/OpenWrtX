4f9b7cb13131ae36891b3a33d5b6a005892bd86e
cpuer <lastczj@gmail.com>
2023-05-14 15:23:04 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Svn checkout packages from immortalwrt's repository
-pushd customfeeds
+# pushd customfeeds
 
 # Add luci-app-eqos
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
+# svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
 
 # Add luci-proto-modemmanager
-svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
+# svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
 
 # Add luci-app-gowebdav
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
-rm -rf packages/net/gowebdav
-svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
+# svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+# rm -rf packages/net/gowebdav
+# svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
 
 # Add tmate
 # git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
@@ -30,16 +30,16 @@ svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/g
 # popd
 
 # Set to local feeds
-pushd customfeeds/packages
-export packages_feed="$(pwd)"
-popd
-pushd customfeeds/luci
-export luci_feed="$(pwd)"
-popd
-sed -i '/src-git packages/d' feeds.conf.default
-echo "src-link packages $packages_feed" >> feeds.conf.default
-sed -i '/src-git luci/d' feeds.conf.default
-echo "src-link luci $luci_feed" >> feeds.conf.default
+# pushd customfeeds/packages
+# export packages_feed="$(pwd)"
+# popd
+# pushd customfeeds/luci
+# export luci_feed="$(pwd)"
+# popd
+# sed -i '/src-git packages/d' feeds.conf.default
+# echo "src-link packages $packages_feed" >> feeds.conf.default
+# sed -i '/src-git luci/d' feeds.conf.default
+# echo "src-link luci $luci_feed" >> feeds.conf.default
 
 # Update feeds
-./scripts/feeds update -a
+# ./scripts/feeds update -a
