1fd1f362054eed46fd292fd4c0a1060cd7e9f401
cpuer <lastczj@gmail.com>
2023-07-09 02:42:58 +0800

    update

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -9,17 +9,17 @@
 #=================================================
 # Clone community packages to package/community
 
-rm -rf package/libs/libnl-tiny
-rm -rf package/kernel/mac80211
-rm -rf package/kernel/mt76
-rm -rf package/network/services/hostapd
-rm -rf package/wwan
-svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/wwan package/wwan
-svn export https://github.com/openwrt/openwrt/trunk/package/libs/libnl-tiny package/libs/libnl-tiny
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/mt76 package/kernel/mt76
+# rm -rf package/libs/libnl-tiny
+# rm -rf package/kernel/mac80211
+# rm -rf package/kernel/mt76
+# rm -rf package/network/services/hostapd
+# rm -rf package/wwan
+# svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/wwan package/wwan
+# svn export https://github.com/openwrt/openwrt/trunk/package/libs/libnl-tiny package/libs/libnl-tiny
+# svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
+# svn export https://github.com/DHDAXCW/lede-rockchip/trunk/package/kernel/mt76 package/kernel/mt76
 # svn export https://github.com/DHDAXCW/openwrt-beta/trunk/package/network/services/hostapd package/network/services/hostapd
-svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+# svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
@@ -130,4 +130,4 @@ cp -f $GITHUB_WORKSPACE/02_network target/linux/x86/base-files/etc/board.d/02_ne
 sed -i 's/5.15/6.1/g' target/linux/x86/Makefile
 rm -rf package/base-files/files/etc/banner
 wget -P package/base-files/files/etc https://raw.githubusercontent.com/DHDAXCW/lede-rockchip/stable/package/base-files/files/etc/banner
-cp -r ../target/linux/generic/pending-6.1/ ./target/linux/generic/
+# cp -r ../target/linux/generic/pending-6.1/ ./target/linux/generic/
