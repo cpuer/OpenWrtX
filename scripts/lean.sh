abd2a5c9b35fb8f0b4cc589fb10e9f625a099f93
cpuer <lastczj@gmail.com>
2023-12-30 12:59:11 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -30,10 +30,10 @@ mkdir package/community
 pushd package/community
 
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
-rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
-rm -rf openwrt-package/verysync
-rm -rf openwrt-package/luci-app-verysync
+# git clone --depth=1 https://github.com/Lienol/openwrt-package
+# rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
+# rm -rf openwrt-package/verysync
+# rm -rf openwrt-package/luci-app-verysync
 
 # Add luci-app-ssr-plus
 # git clone --depth=1 https://github.com/fw876/helloworld
